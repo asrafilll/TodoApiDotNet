e54b8f8e38ac4af4fe3c72ac68832f3c935d24c8
Asrafil NCI <asrafilr@gmail.com>
2024-01-10 10:48:50 -0800

    add new controller

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9edcc781bf0c3493f97bd957a047b49affe31270")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
